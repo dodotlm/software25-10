13b2a53ec3ddecab16cd1290041c23bb745ebe1d
아이디 <dlehdals0987@gmail.com>
2025-05-09 11:18:55 +0900

    Fix commit error

--- InRang/InRang/Form1.cs
+++ InRang/InRang/Form1.cs
@@ -16,5 +16,10 @@ namespace InRang
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
